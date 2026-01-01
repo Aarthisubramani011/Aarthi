bcffa883ef38dea7fe125dbc512bc51e98431e2f
aarthisubramni <aarthisubramani1973@gmail.com>
2026-01-01 23:56:14 +0530

    ADDED: Code practice

---
+++ solution/Code_Sessions/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+Console.Write("Enter an value:");
+int nInput = Convert.ToInt32(Console.ReadLine());
+while (nInput != 1)
+{
+    Console.Write( nInput +" ");
+    if (nInput % 2 == 0)
+    {
+        nInput = nInput / 2;
+    }
+    else
+    {
+        nInput = nInput * 3 +1;
+    }
+}
+Console.WriteLine(nInput);
