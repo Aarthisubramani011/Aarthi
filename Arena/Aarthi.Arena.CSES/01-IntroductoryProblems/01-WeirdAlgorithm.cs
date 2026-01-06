e76a599addfe476a694661b5c57b189745946c4c
Aarthi <aarthisubramani1973@gmail.com>
2026-01-06 18:41:59 +0530

    ADDED: Solvedproblems

---
+++ Arena/Aarthi.Arena.CSES/01-IntroductoryProblems/01-WeirdAlgorithm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Aarthi.Arena.CSES {
+    class Weird_algorithm
+    {
+        public static void Algorithm()
+        {
+            Console.Write("Enter an value:");
+            int nInput = Convert.ToInt32(Console.ReadLine());
+            while (nInput != 1)
+            {
+                Console.Write(nInput + " ");
+                if (nInput % 2 == 0)
+                {
+                    nInput = nInput / 2;
+                }
+                else
+                {
+                    nInput = nInput * 3 + 1;
+                }
+            }
+            Console.WriteLine(nInput);
+        }
+    }
+}
+
+
+
---
+++ Arena/Aarthi.Arena.CSES/01-IntroductoryProblems/04-Increasing_array.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aarthi.Arena.CSES
+{
+    class _04_IncreaceArray
+    {
+        public static void IncreasingArray()
+        {
+            Console.Write("Enter an numbeer of values:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Enter {n} values");
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Enter value{i}:");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int  moves = 0;
+            for (int i = 1; i < n; i++)
+            {
+                if (arr[i] > arr[i - 1])
+                {
+                    moves += arr[i - 1] - arr[i];
+                    arr[i] = arr[i - 1];
+                }
+                
+            }
+            Console.WriteLine("No. of moves ", moves);
+
+
+
+
+        }
+    }
+}
+      
+              
+          
+
