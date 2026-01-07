72ac802860995b6e159ec8880885867526f905fe
Aarthi <aarthisubramani1973@gmail.com>
2026-01-07 18:56:49 +0530

    ADDED: Added Team problem

---
+++ Arena/Aarthi.Arena.CSES/02- CodeForces/03-Team.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aarthi.Arena.CSES._02__CodeForces
+{
+    public class _03_Team
+    {
+        public static void _Team()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int result = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int tmp = 0;
+                string ans= Console.ReadLine();
+              
+                 for (int j=0; j< ans.Length; j++)
+                {
+                    if (ans[j] == '1' )
+                    {
+                        tmp += 1;
+
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                 }
+
+                 if (tmp >= 2){
+                    result += 1;
+                 }
+                
+
+            }
+            Console.WriteLine(result);
+
+        }
+    }
+}
