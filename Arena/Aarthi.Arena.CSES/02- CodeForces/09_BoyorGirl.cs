1d607c69f7be37546faa3aa3f638ddc57759fa2b
Aarthi <aarthisubramani1973@gmail.com>
2026-02-03 18:33:03 +0530

    ADDED:Added boygirl problem

---
+++ Arena/Aarthi.Arena.CSES/02- CodeForces/09_BoyorGirl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aarthi.Arena.CSES._02__CodeForces
+{
+    public class YESorYES
+    {
+        public static void YESorYES__()
+        {
+            
+            string user = Console.ReadLine();
+            HashSet<char> set = new HashSet<char>();
+            foreach ( char c in user)
+            {
+                set.Add(c);
+            }
+          
+            if (set.Count % 2 == 0)
+            {
+                Console.WriteLine("CHAT WITH HER!\r\n");
+
+            }
+            else
+            {
+                Console.WriteLine("IGNORE HIM!\r\n");
+            }
+        }
+    }
+}
